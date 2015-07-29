16c473234af051c7835b7daf384612403f3dd442
scrcls <shicanrongcls@gmail.com>
2015-07-29 02:32:24 +0000

    complete contact parse

--- necontacts/nanny_contacts.py
+++ necontacts/nanny_contacts.py
@@ -9,29 +9,44 @@ def parse_contact(filename):
     
     soup = bs(contact_data, 'lxml')
     contact_table = soup.find('table', attrs = {'class' : 'table table-striped'})
+    contact_list = []
     for contact_info in contact_table.find_all('tr')[1:]:
         contact_info = contact_info.find_all('td')
         empno = contact_info[0].text
         name = contact_info[1].text.encode('utf-8')
         mail = contact_info[2].text
         dept = contact_info[3].text.encode('utf-8')
-        if name.endswith('(disabled)'):
-            print name, dept
+        if not name.endswith('(disabled)'):
+            contact_list.append({'姓名' : name, 
+                                '电子邮件' : mail,
+                                '部门' : dept
+                                })
+    #for item in contact_dict:
+    #    for key, val in item.iteritems():
+    #        print key, val
+    return contact_list
         
 
-def write_csv(contact_data, out_file):
-    pass
+def write_csv(contact_header, contact_list, out_file):
+    with open(out_file, 'wb') as myfile:
+        writer = csv.DictWriter(myfile, fieldnames = contact_header,
+                                delimiter = ',', quotechar = '\"',
+                                quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        for contact_info in contact_list:
+            writer.writerow(contact_info)
 
 def read_csv(in_file):
     with open(in_file, 'rb') as myfile:
-        reader = csv.DictReader(myfile)
-        for row in reader:
-            print row.keys()[0]
-    
+        reader = csv.reader(myfile, delimiter = ',', quotechar = '"')
+        field_names = reader.next()
+        return field_names
+
 if __name__ == '__main__':
     contact_file = './contacts.html'
-    address_file = './address.csv'
-    read_csv(address_file)
-    #parse_contact(contact_file)
+    address_file = './address.txt'
+    contact_header = read_csv(address_file)
+    contact_list = parse_contact(contact_file)
+    write_csv(contact_header, contact_list, 'result.csv')
 
     
